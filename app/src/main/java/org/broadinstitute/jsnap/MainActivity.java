e25fda770574347d77824be45c8d15f0c84e75c2
Amr Abouelleil <amr@dry-kidz.com>
2017-09-26 10:29:59 -0400

    build fixes

--- app/src/main/java/org/broadinstitute/jsnap/MainActivity.java
+++ app/src/main/java/org/broadinstitute/jsnap/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.btnCamera);
     }
 
     private void dispatchTakePictureIntent() {
