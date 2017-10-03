bd2fa8cec741d21e65ae2112d700f57af182b75b
Amr Abouelleil <amr@broadinstitute.org>
2017-10-03 16:12:12 -0400

    -Camera functionality is working now.

--- app/src/main/java/org/broadinstitute/jsnap/MainActivity.java
+++ app/src/main/java/org/broadinstitute/jsnap/MainActivity.java
@@ -2,11 +2,17 @@ package org.broadinstitute.jsnap;
 
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,18 +23,22 @@ import android.widget.ImageView;
 import android.widget.Toast;
 import java.io.File;
 import java.io.IOException;
+import java.util.jar.Manifest;
 
 
 public class MainActivity extends Activity {
     private static String logtag = "MainActivity";
     private static int TAKE_PICTURE = 1;
-    private Uri imageURI;
 
-//    static final int REQUEST_IMAGE_CAPTURE = 1;
-//    private String mCurrentPhotoPath;
-//
+    private static final String AUTHORITY = BuildConfig.APPLICATION_ID+".fileprovider";
+
+    private static final String PHOTOS="photos";
+    private static final String FILENAME="jsnap_test.jpeg";
+    Uri imageURI;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button cameraButton = (Button)(findViewById(R.id.cam_button));
@@ -39,14 +49,17 @@ public class MainActivity extends Activity {
             try {
                 takePhoto(v);
             } catch (Exception e) {
+                Log.e(logtag, getExternalFilesDir(Environment.DIRECTORY_PICTURES).getPath());
                 Log.e(logtag, e.toString());
+                Log.e(logtag, "exception", e);
+
             }
         }
     };
     private void takePhoto(View v){
         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        File photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "jsnap.jpg");
-        imageURI = Uri.fromFile(photo);
+        File photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), FILENAME);
+        imageURI=FileProvider.getUriForFile(this, AUTHORITY, photo);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, imageURI);
         // TAKE_PICTURE is a request code saying we want to use the rear-facing camera.
         startActivityForResult(intent, TAKE_PICTURE);
@@ -73,51 +86,4 @@ public class MainActivity extends Activity {
             }
         }
     }
-
-//    private void dispatchTakePictureIntent() {
-//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-//            File photoFile = null;
-//            try {
-//                photoFile = createImageFile();
-//            } catch (IOException e) {
-//                // Error occured while creating the File. Do something about it.
-//
-//            }
-//            if (photoFile != null) {
-//                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider",
-//                        photoFile);
-//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-//            }
-//        }
-//    }
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        ImageView mImageView;
-//        mImageView = (ImageView) findViewById(R.id.imageView);
-//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-//            Bundle extras = data.getExtras();
-//            Bitmap imageBitmap = (Bitmap) extras.get("data");
-//            // Need to set up an imageview
-//            mImageView.setImageBitmap(imageBitmap);
-//        }
-//    }
-//
-//    private File createImageFile() throws IOException {
-//        // Create an image file name
-//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-//        String imageFileName = "JPEG_" + timeStamp + "_";
-//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-//        File image = File.createTempFile(
-//                imageFileName,  /* prefix */
-//                ".jpg",         /* suffix */
-//                storageDir      /* directory */
-//        );
-//
-//        // Save a file: path for use with ACTION_VIEW intents
-//        mCurrentPhotoPath = image.getAbsolutePath();
-//        return image;
-//    }
-
 }
